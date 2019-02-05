f0000a68c6868694e62c49af9869cd2965ba53ad
Alex de los Santos <adelossantos@topsystemscorp.com>
2019-02-05 17:14:00 -0300

    Fix initialState
    Se mueve la lista "search" para afuera del objeto "data"

--- src/entries/home.js
+++ src/entries/home.js
@@ -8,7 +8,8 @@ import reducer from '../reducers/data'
 
 // descomponemos la data en un nuevo objeto de un único atributo también llamado data.
 const initialState = {
-  data: {...data}
+  data: {...data },
+  search: []
 }
 
 const store = createStore (
