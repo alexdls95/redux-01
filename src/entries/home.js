15f595078fef28b164f2ed720cbc1b92dd6cc685
Alex de los Santos <adelossantos@topsystemscorp.com>
2019-02-05 15:01:51 -0300

    Creando el Store para nuestro reproductor de video
    Lo mismo que ya se hizo antes, pero ahora dentro de nuestra aplicación que utiliza React.

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,6 +2,20 @@ import React from 'react'
 import { render } from 'react-dom'
 import Home from '../pages/containers/home'
 import data from '../../src/api.json'
+import { createStore } from 'redux'
+
+// descomponemos la data en un nuevo objeto de un único atributo también llamado data.
+const initialState = {
+  data: {...data}
+}
+
+const store = createStore (
+  (state)=>state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // este enhancer sirve para debug desde el navegador.
+)
+
+console.log(store.getState())
 
 const homeContainer = document.getElementById('home-container')
 
