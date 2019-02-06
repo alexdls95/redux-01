258fc51ec84fe243e5b89ac84d25887282f05b62
Alex de los Santos <adelossantos@topsystemscorp.com>
2019-02-06 12:08:02 -0300

    Creando otro reducer y combinándolos todos
    Se crea un reducer para manejar los modales y se combinan ambos reducers en un archivo sólo, utilizando combineReducer de redux. A su vez, también se envía el estado inicial que utiliza cada reducer a su respectivos archivos, y se pasan esos estados como parámetros por defecto.
    Por otro lado y como cambio menor: se pasa el key "search" de nuestro initialState para dentro de la key "data", sólo para efectos de correcta estructuración.

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,24 +3,11 @@ import { render } from 'react-dom'
 import Home from '../pages/containers/home'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from '../reducers/data'
-import data from '../schemas/index'
-
-console.log(data)
-
-
-// descomponemos la data en un nuevo objeto de un único atributo también llamado data.
-const initialState = {
-  data: {
-    entities: data.entities,
-    categories: data.result.categories,
-  },
-  search: []
-}
+import reducer from '../reducers/index'
 
 const store = createStore (
   reducer,
-  initialState,
+  {},
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // este enhancer sirve para debug desde el navegador.
 )
 
---
+++ src/reducers/modal.js
@@ -0,0 +1,18 @@
+const initialState = {
+    visible: false,
+    mediaId: null,
+}
+
+
+function modal (state=initialState, action) {
+  switch(action.type) {
+    case 'OPEN_MODAL':
+      return state
+    case 'CLOSE_MODAL':
+      return state
+    default:
+      return state
+  }
+}
+
+export default modal
