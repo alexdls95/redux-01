ce43e509934c256c6310c75031c5ad1f30e8e317
Alex de los Santos <adelossantos@topsystemscorp.com>
2019-02-07 17:55:16 -0300

    Action Types
    Hemos transformado nuestros string de acciones en constantes, que las exportaremos desde un archivo específico que contendrá todos los tipos de acciones de nuestra aplicación. Esto nos permitirá ahorrarnos 'typos' al momento de escribir los tipos de acciones existentes.

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -1,5 +1,5 @@
 import { fromJS } from 'immutable'
-
+import { OPEN_MODAL, CLOSE_MODAL } from '../action-types/index'
 const initialState = fromJS({
     visible: false,
     mediaId: null,
@@ -8,12 +8,12 @@ const initialState = fromJS({
 
 function modal (state=initialState, action) {
   switch(action.type) {
-    case 'OPEN_MODAL':
+    case OPEN_MODAL:
       return state.merge({
         'visible': true,
         'mediaId': action.payload.mediaId
       })
-    case 'CLOSE_MODAL':
+    case CLOSE_MODAL:
       return state.set('visible', false)
     default:
       return state
